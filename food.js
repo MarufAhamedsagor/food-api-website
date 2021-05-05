19b637fe83203b7a44aced8e731dd49cec5153e5
Maruf Ahamed Sagor <maruf2007ahamed1206@gmail.com>
2021-05-06 02:37:19 +0600

    last editing

--- food.js
+++ food.js
@@ -17,15 +17,11 @@ const displayMealItem = (mealInfo) => {
           const mealItemBox = document.createElement('div');
           mealItemBox.className = "mealTitle";
           const foodInfo = `
-            <div onclick="displayDetails('${food.idMeal}')">
+            <div onclick="displayDetails('${foodElement.idMeal}')">
             <img src= "${foodElement.strMealThumb}" />
             <h3 class="food-name">${foodElement.strMeal}</h3>
             </div>
           `;
-          // const foodInfo =`
-          // <img src= "${foodElement.strMealThumb}" />
-          // <h3 class="food-name">${foodElement.strMeal}</h3>
-          // `
           mealItemBox.innerHTML = foodInfo;
           recipeBox.appendChild(mealItemBox);
          }); 
@@ -49,7 +45,16 @@ const renderFoodInfo = food => {
       const foodDivDetails = document.getElementById('food-details');
       foodDivDetails.innerHTML = `
       <img src="${food.strMealThumb}" alt="">
-      
+      <h4>${food.strMeal}</h4>
+      <h5>Ingredients</h5>
+      <ul>
+      <li>${food.strMeasure1}, ${food.strIngredient1}</li>
+      <li>${food.strMeasure2}, ${food.strIngredient2}</li>
+      <li>${food.strMeasure3}, ${food.strIngredient3}</li>
+      <li>${food.strMeasure4}, ${food.strIngredient4}</li>
+      <li>${food.strMeasure5}, ${food.strIngredient5}</li>
+      <li>${food.strMeasure6}, ${food.strIngredient6}</li>
+      </ul>
       `
 }
 
